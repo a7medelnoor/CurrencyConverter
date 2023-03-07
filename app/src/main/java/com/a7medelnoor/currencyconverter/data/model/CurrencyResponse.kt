bf21e5b63d36fa255b6a181c3ac8a872547ba24b
ahmed <acc.ahmedstudio@gmail.com>
2023-03-07 12:33:35 +0400

    remove room annotation

--- app/src/main/java/com/a7medelnoor/currencyconverter/data/model/CurrencyResponse.kt
+++ app/src/main/java/com/a7medelnoor/currencyconverter/data/model/CurrencyResponse.kt
@@ -1,13 +1,6 @@
 package com.a7medelnoor.currencyconverter.data.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
 data class CurrencyResponse(
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "rate")
     val rates: Rates,
     val result: String
 )
