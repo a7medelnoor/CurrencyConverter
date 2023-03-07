f7ebe4ed9dbc8fffb7206e3b985275bc8a28a29c
ahmed <acc.ahmedstudio@gmail.com>
2023-03-07 10:25:49 +0400

    create network module and provide okhhtpclient for retrofit, and provide api service to our application

---
+++ app/src/main/java/com/a7medelnoor/currencyconverter/di/NetworkModule.kt
@@ -0,0 +1,46 @@
+package com.a7medelnoor.currencyconverter.di
+
+import com.a7medelnoor.currencyconverter.data.api.ApiService
+import com.a7medelnoor.currencyconverter.util.BASE_URL
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+object NetworkModule {
+
+
+    // provide ok http
+    @Singleton
+    @Provides
+    fun provideOkHttp(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .build()
+    }
+
+
+    // provide retrofit
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+
+    // provide api service
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
